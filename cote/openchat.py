03f1c1863eec96334a63ca0c659a0b885549d710
Jee Yoo <yoojw94@gmail.com>
2021-11-20 23:04:23 +0900

    Update openchat.py

--- cote/openchat.py
+++ cote/openchat.py
@@ -1,6 +1,14 @@
+def korean_verb(any): 
+    if (any == 'Enter'):
+        return '님이 들어왔습니다.'
+    elif (any == 'Leave'): return '님이 나갔습니다.'
+
 def solution(record):
     answer = []
     
-    for 
+    for sentences in range(len(record)):
+        words = record[sentences].split()
+        explanation = korean_verb(words[0])
+        answer.append(words[1]+explanation)
     
     return answer
