23a1f72d3ed2e40a119998ef9c5a111740dd39ed
Jee Yoo <yoojw94@gmail.com>
2022-03-14 21:51:18 +0900

    Update ex1.sql

--- sql/ex1.sql
+++ sql/ex1.sql
@@ -1,3 +1,23 @@
 /*상위 1000개*/
 SELECT TOP (1000) [CUST_ID], [BIRTH_DTH]
 FROM [MYDB1]
+
+CREATE TABLE [Employee]
+(
+    [EmployeeId] INTEGER  NOT NULL,
+    [LastName] NVARCHAR(20)  NOT NULL,
+    [FirstName] NVARCHAR(20)  NOT NULL,
+    [Title] NVARCHAR(30),
+    [ReportsTo] INTEGER,
+    [BirthDate] DATETIME,
+    [HireDate] DATETIME,
+    [Address] NVARCHAR(70),
+    [City] NVARCHAR(40),
+    [State] NVARCHAR(40),
+    [Country] NVARCHAR(40),
+    [PostalCode] NVARCHAR(10),
+    [Phone] NVARCHAR(24),
+    [Fax] NVARCHAR(24),
+    [Email] NVARCHAR(60)
+  
+);
