7c556650ac4e548dc0e619d74f1d70556de00681
Jee Yoo <yoojw94@gmail.com>
2022-09-07 21:35:01 +0900

    Update and rename sql_b1.sql to sql_dml1.sql

--- languages/sql/sql_b1.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO db1.table1 (name, dept_cd, phone, address) VALUES ('Camile', 'C', '032457899', 'Brisbane');
---
+++ languages/sql/sql_dml1.sql
@@ -0,0 +1,5 @@
+INSERT INTO db1.table1 (name, dept_cd, phone, address) VALUES ('Camile', 'C', '032457899', 'Brisbane');
+
+INSERT INTO Persons VALUES (1, 'b', 80, 'd', '98128');
+INSERT INTO Persons VALUES (2, 'c', 90, 'd', '98128');
+INSERT INTO Persons VALUES (3, 'd', 80, 'd', '98128');
