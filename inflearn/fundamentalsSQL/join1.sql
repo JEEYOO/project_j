16d797ce2d3b4ca25e73761c8a70d457bc425b8a
Jee Yoo <yoojw94@gmail.com>
2024-03-24 20:53:40 +0900

    Update join1.sql

--- inflearn/fundamentalsSQL/join1.sql
+++ inflearn/fundamentalsSQL/join1.sql
@@ -1,4 +1,17 @@
+
 select e.*, d.dname
 from hr.emp e 
   join hr.dept d on e.deptno = d.deptno;
+where e.job = 'SALESMAN'
 
+  
+
+select * from hr.emp_salary_hist;  
+
+
+
+select a.dname, b.empno, b.ename, b.job, c.fromdate, c.todate, c.sal
+from hr.dept a
+  join hr.emp b on a.deptno = b.deptno
+  join hr.emp_salary_hist c on b.empno = c.empno   
+where a.dname in ('SALES', 'RESEARCH')
