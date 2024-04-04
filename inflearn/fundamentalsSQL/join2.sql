f892f7c8b5180183ad2f9acd1f7f00e3cb988095
Jee Yoo <yoojw94@gmail.com>
2024-04-04 23:26:04 +0900

    Update join2.sql

--- inflearn/fundamentalsSQL/join2.sql
+++ inflearn/fundamentalsSQL/join2.sql
@@ -3,3 +3,4 @@ from nw.customers cust
 join nw.orders ord on cust.customer_id = ord.customer_id
 where cust.contact_name = 'Antonio Moreno'
 and ord.order_date between to_date('19970101', 'yyyymmdd') and to_date('19971231', 'yyyymmdd');
+-- to_date function
