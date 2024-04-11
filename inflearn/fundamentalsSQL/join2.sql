09fb26b549ef6406341283e84e5e0f9cd56f32d4
Jee Yoo <yoojw94@gmail.com>
2024-04-11 22:37:18 +0900

    Update join2.sql

--- inflearn/fundamentalsSQL/join2.sql
+++ inflearn/fundamentalsSQL/join2.sql
@@ -10,3 +10,12 @@ select count(*) --c.customer_id, c.contact_name, o.order_id, o.customer_id
 from nw.customers c
    --left outer join nw.orders o on c.customer_id = o.customer_id
    join nw.orders o on c.customer_id = o.customer_id 
+
+
+-- coalsece(빈 값), ||
+select c.customer_id, c.contact_name, coalesce(o.order_id, 0) as order_id, e.first_name ||' '||e.last_name as employee_name, s.company_name as shipper_name
+from nw.customers c
+   left join nw.orders o on c.customer_id = o.customer_id 
+   left join nw.employees e on o.employee_id = e.employee_id 
+   left join nw.shippers s on o.ship_via = s.shipper_id 
+where c.city = 'Madrid';
