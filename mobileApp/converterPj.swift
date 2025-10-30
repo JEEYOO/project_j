98096d298eb989f720ec7a2a89350c9cac76fb53
Jee Yoo <yoojw94@gmail.com>
2025-10-30 20:44:03 +0900

    Refactor currency exchange UI in converterPj.swift

---
+++ mobileApp/converterPj.swift
@@ -0,0 +1,93 @@
+//  Created by H on 10/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var showExchangeInfo = false
+    @State var leftAmount = ""
+    @State var rightAmount = ""
+    
+    var body: some View {
+                ZStack {
+            // Background image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                //Prancing pony image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                //Text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                
+                HStack {
+                    VStack {
+                        HStack {
+                            Image(.silverpenny)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height:33)
+                                
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        TextField("Amount", text: $leftAmount)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+                    VStack {
+                        HStack {
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height:33)
+                            
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        TextField("Amount", text: $rightAmount)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                }
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        showExchangeInfo.toggle() //boolean True False
+                        print("showExchangeInfo value: \(showExchangeInfo)")
+                    } label : {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.trailing)
+                }
+                
+                
+                
+                
+            }
+//          .border(.blue)
+        }
+    }    
+}
+
+#Preview {
+    ContentView()
+}
