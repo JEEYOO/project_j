2ef944a5059b6c2b640bc562f8ad5cc9e91a0648
Jee Yoo <yoojw94@gmail.com>
2025-11-04 16:07:53 +0900

    Refactor currency exchange display in ExchangeInfo
    Updated currency exchange display to use ExchangeRate view.

--- mobileApp/converterPj/ExchangeInfo.swift
+++ mobileApp/converterPj/ExchangeInfo.swift
@@ -16,7 +16,8 @@ struct ExchangeInfo: View {
                 Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
                     .font(.title3)
                     .padding()
-                
+
+/*Use ExchangeRate view instead
                 HStack {
                     Image(.goldpiece)
                         .resizable()
@@ -30,8 +31,11 @@ struct ExchangeInfo: View {
                         .scaledToFit()
                         .frame(height:33)
                 }
-
-                ExtractedView()
+*/
+                ExtractedView(leftImage: .goldpiece, text: "1 Gold Piece = 4 Gold Pennies", rightImage: .goldpenny)
+                ExchangeRate(leftImage: .goldpenny, text: "1 Gold Penny = 4 Silver Pieces", rightImage: .silverpiece)
+                ExchangeRate(leftImage: .silverpiece, text: "1 Silver Piece = 4 Silver Pennies", rightImage: .silverpenny)
+                ExchangeRate(leftImage: .silverpenny, text: "1 Silver Penny = 100 Copper Pennies", rightImage: .copperpenny)
                 
                 Button("Done") {
                     
