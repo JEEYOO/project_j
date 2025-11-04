f44a3459f0ad78a37ae8a6e30df2f6cd5989f66b
Jee Yoo <yoojw94@gmail.com>
2025-11-04 16:06:50 +0900

    Add ExchangeRate view for displaying currency exchange

---
+++ mobileApp/converterPj/ExchangeRate.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+struct ExchangeRate: View {
+    let leftImage: ImageResource
+    let text: String
+    let rightImage: ImageResource
+    
+    var body: some View {
+        HStack {
+            Image(leftImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height:33)
+            
+            Text(text)
+            
+            Image(rightImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height:33)
+        }
+    }
+}
+
+#Preview {
+    ExchangeRate(leftImage: .silverpiece, text: "1 Silver Piece = 4 Silver Pieces", rightImage: .silverpenny)
+}
+
