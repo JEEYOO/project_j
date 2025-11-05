d673f5d5fe60b2e5c68636a8537881e683b3bb74
Jee Yoo <yoojw94@gmail.com>
2025-11-05 16:14:08 +0900

    Update ExchangeInfo.swift

--- mobileApp/converterPj/ExchangeInfo.swift
+++ mobileApp/converterPj/ExchangeInfo.swift
@@ -17,7 +17,7 @@ struct ExchangeInfo: View {
                     .font(.title3)
                     .padding()
 
-                ExtractedView(leftImage: .goldpiece, text: "1 Gold Piece = 4 Gold Pennies", rightImage: .goldpenny)
+                ExchangeRate(leftImage: .goldpiece, text: "1 Gold Piece = 4 Gold Pennies", rightImage: .goldpenny)
                 ExchangeRate(leftImage: .goldpenny, text: "1 Gold Penny = 4 Silver Pieces", rightImage: .silverpiece)
                 ExchangeRate(leftImage: .silverpiece, text: "1 Silver Piece = 4 Silver Pennies", rightImage: .silverpenny)
                 ExchangeRate(leftImage: .silverpenny, text: "1 Silver Penny = 100 Copper Pennies", rightImage: .copperpenny)
