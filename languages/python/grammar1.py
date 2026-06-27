851dac292f0f39426aa860a6e72d6ca4335552f3
Jee Yoo <yoojw94@gmail.com>
2026-06-27 18:33:38 +0900

    Add basic Python grammar examples

---
+++ languages/python/grammar1.py
@@ -0,0 +1,18 @@
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.")
+#input("Can I get your name?")
+#print("Hello " + input("Nationality"))
+print("Abcdef"[0])
+print(21_475_093_432)
+
+import random
+
+listA = ['A','B','C','D','E','F']
+
+print(random.choice(listA))
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen[1][1])
