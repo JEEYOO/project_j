b6c41c4730c5fe10624d41368d9e4720987e583a
Jee Yoo <yoojw94@gmail.com>
2026-07-05 15:52:40 +0900

    Style data table with currency formatting
    Added currency formatting and style to the price column in the data table.

--- languages/R/dataTable.R
+++ languages/R/dataTable.R
@@ -7,7 +7,10 @@ server <- function(input, output, session) {
   output$tableDT <- DT::renderDataTable(diamonds[1:1000,],
                                         options = list(paging=F),
                                         rownames = F, #false
-                                        filter = "top")
+                                        filter = "top") %>%
+                                      formatCurrency("price", "$") %>%
+                                      
+                                      formatStyle("price", color = "green")
    
 }
 
