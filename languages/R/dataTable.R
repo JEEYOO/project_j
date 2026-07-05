7d6dcca4dd3d976bd495d04a07d50f1aaa4d4bc1
Jee Yoo <yoojw94@gmail.com>
2026-07-05 15:21:10 +0900

    Add Shiny app with DataTable for diamonds dataset

---
+++ languages/R/dataTable.R
@@ -0,0 +1,18 @@
+library(shiny)
+library(ggplot2)
+library(DT)
+
+server <- function(input, output, session) {
+  
+  output$tableDT <- DT::renderDataTable(diamonds[1:1000,],
+                                        options = list(paging=F),
+                                        rownames = F, #false
+                                        filter = "top")
+   
+}
+
+ui <- fluidPage(
+  DT::dataTableOutput("tableDT")
+)
+
+shinyApp(ui = ui, server = server)
