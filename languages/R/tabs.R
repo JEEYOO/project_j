be3bc55f2ed2bb461470c7c7d2964bfc4267eb15
Jee Yoo <yoojw94@gmail.com>
2026-07-11 12:57:10 +0900

    Update tabs.R

--- languages/R/tabs.R
+++ languages/R/tabs.R
@@ -8,6 +8,13 @@ ui = navbarPage("Integration of different media types",
   
   tabPanel("Image sourced locally",
            tags$img(src = "noOtherChoice.jpg", width = "100px", height= "100px")) ,             
+
+  tabPanel("video sourced locally", 
+           tags$video(src = "hbd.mp4", type = "video/mp4", controls = T, width = "900px", height = "800px")),
+                
+  tabPanel("Pdf sourced online, Iframe",
+           tags$iframe(style="height:600px; width:100%; scrolling=yes",
+                       src="resume.pdf")), 
                 
   tabPanel("Text as .txt",
            includeText("dcbackup_all_auto.txt"))                
