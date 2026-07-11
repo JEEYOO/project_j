6f7fdfedccc0a36acbce7a78ec26c5d0858fea23
Jee Yoo <yoojw94@gmail.com>
2026-07-11 11:42:41 +0900

    Implement download button for CSV export
    Added a download button to export data as CSV.

--- languages/R/dt_brush.R
+++ languages/R/dt_brush.R
@@ -15,12 +15,20 @@ server <- function(input, output, session) {
   })
   
   output$table <- DT::renderDataTable(DT::datatable(diam()))
+
+  output$download1 <- downloadHandler(
+    filename = "extractPlot.csv", 
+    content = function(file) {
+      write.csv(diam(), file)
+    }
+  )
 }
 
 ui <- fluidPage(
   h1("Brush feature"),
   plotOutput("plot", brush = "user_brush"),
-  dataTableOutput("table")
+  dataTableOutput("table"),
+  downloadButton(outputId = "download1", label = "Download")
 )
 
 shinyApp(ui = ui, server = server)
